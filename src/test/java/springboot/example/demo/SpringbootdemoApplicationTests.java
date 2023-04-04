fe5543ff4ea9a44e348ac8a4994186198e4bf468
Janis <janis.odins92@gmail.com>
2023-04-04 19:09:15 +0300

    first commit

---
+++ src/test/java/springboot/example/demo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
